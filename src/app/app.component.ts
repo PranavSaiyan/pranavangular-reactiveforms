48a1482ed288f29f798bffda413057f611e068bd
PranavSaiyan <pranavsupersaiyan10@gmail.com>
2019-04-26 00:29:04 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit} from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+  myForm: FormGroup;
   genders = ['Male','Female','Others'];
   answer = '';
   formUser = { 
     name : '',
     email:'',
     password:'',
-    gender:''
-    ,answer:''
+    gender:'',
+    answer:''
   };
+
+  ngOnInit(){
+    this.myForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'password': new FormControl(null),
+      'gender': new FormControl('Male'),
+      'list': new FormControl('book'),
+      'answer': new FormControl(null)
+    });
+  }
 }
